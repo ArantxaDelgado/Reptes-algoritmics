3407b88b15da95c8680ff30d99aa472fc4549e13
ardelgad <adelgadoporras@gmail.com>
2022-03-10 08:03:26 +0100

    clase

---
+++ ADNcomplementari.js
@@ -0,0 +1,16 @@
+// Kata: https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+
+// La universidad de Michichuguin nos ha pedido un software que calcula el secuencia ADN complementaría a otra
+// A<-->T C<--->G . El complementario de A es T y viceversa. El complementario de C es G y viceversa.
+
+function DNAStrand(dna){
+  let sequence = {
+    "A": "T",
+    "T": "A",
+    "G": "C",
+    "C": "G"
+  }
+  return dna.replace(/A|T|G|C/g, function(matched){
+    return sequence[matched];
+  });
+}
