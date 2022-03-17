9e47cdd9492fdd1d8404fbc25b785f3e66289542
ardelgad <adelgadoporras@gmail.com>
2022-03-17 20:14:59 +0100

    examCheckReplaceSpace

--- examCorrection.js
+++ examCorrection.js
@@ -33,6 +33,6 @@ function checkExam(array1, array2) {
     };
 
 console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]), 6);
-/console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]), 7);
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]), 7);
 console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]), 16);
 console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]), 0);
